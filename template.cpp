eff26b858c2c39bf593c037779cd65941166b389
Vignesh Iyer <39982819+VigneshIyer25@users.noreply.github.com>
2022-06-15 15:10:44 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define pb push_back
+#define sz(a)   (int)a.size()
+#define mp make_pair
+#define F first
+#define S second
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const int N = 3e5;
+
+vi g[N];
+int a[N];
+
+int main() {
+    int t, i, j, k, p, q, r, x, y, z, n;
+    si(t);
+    while(t--){
+        cin >> n;
+        fo(i, n) cin >> a[i];
+    }
+    return 0;
+}
