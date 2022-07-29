d7b9e88b80a58820a2d2f178332f38af451a1982
Vignesh Iyer <1792167@tcs.com>
2022-07-29 12:10:11 +0530

    usaco submission

--- usaco_submissions/namenum.py
+++ usaco_submissions/namenum.py
@@ -1,13 +1,16 @@
-import sys
-
-'''
+"""
 TASK: namenum
 USER: Vignesh Iyer
-LANG: Python
-'''
+LANG: PYTHON3
+"""
+
+def read_input(filename):
+    global id_
+    with open(filename, 'r') as fin:
+        id_ = fin.readline().strip()
+
 hm = {'A':2, 'B': 2, 'C':2, 'J':5, 'K':5, 'L':5, 'T':8,'U':8,'V':8, 'D':3, 'E':3, 'F':3, 'M':6, 'N':6, 'O':6, 'W':9,'X':9,'Y':9, 'G':4, 'H':4, 'I':4, 'P':7, 'R':7, 'S':7}
 
-# returns a list of names mapped with their appropriate id
 def read_dictionary(filename) -> list:
     with open(filename, 'r') as f:
         valid_names = f.read().split()
@@ -22,26 +25,24 @@ def read_dictionary(filename) -> list:
         res[name] = idx
     return res
 
-def solve():
-    idx = str(input())
+def solve(fout):
     valid_ids = read_dictionary('dict.txt')
     ans = []
     
     for key, val in valid_ids.items():
-        if val == idx:
+        if val == id_:
             ans.append(key)
     
     if(ans == []):
-      print("NONE")
+      fout.write("NONE\n")
     else:
       ans.sort()
       for name in ans:
-          print(name)
+          fout.write(name+'\n')
 
-if __name__ == "__main__":
-    sys.stdin = open('namenum.in', 'r')
-    sys.stdout = open('namenum.out', 'w')
-    tc = 1
-    while(tc):
-        solve()
-        tc -= 1
+filename = 'namenum'
+read_input(filename+'.in')
+
+## output
+with open(filename+'.out', 'w') as fout:
+    solve(fout)
