49036151ac1e3850fa0631318997956a04d957e0
Vignesh Iyer <1792167@tcs.com>
2022-08-12 13:27:37 +0530

    refactored usaco directory

--- main.cpp
+++ main.cpp
@@ -35,75 +35,10 @@ void read_input(string filename){
     freopen((filename + ".out").c_str(), "w", stdout);
 }
 
-int n;
-vi a;
-int med;
-priority_queue<double> s;
-priority_queue<double,vector<double>,greater<double>> g;
 
-int get_median(int x, int pos){
-    if(pos == 1){
-        s = priority_queue<double>(); // reset priority queue
-        g = priority_queue<double,vector<double>,greater<double>>();
-        med = x;
-        s.push(x);
-        return med;
-    }
-    if (s.size() > g.size()){
-        if (x < med){
-            g.push(s.top());
-            s.pop();
-            s.push(x);
-        }
-        else
-            g.push(x);
-        med = min(s.top(),g.top());
-    }
-    else if (s.size()==g.size()){
-        if (x < med){
-            s.push(x);
-            med = s.top();
-        }
-        else{
-            g.push(x);
-            med = g.top();
-        }
-    }
-    else{
-        if (x > med){
-            s.push(g.top());
-            g.pop();
-            g.push(x);
-        }
-        else
-            s.push(x);
-        med = min(s.top(), g.top());
-    }
-    return med;
-}
-
-bool is_valid(vi arr){
-    int p1 = get_median(arr[0], 1);
-
-    for(int i = 1; i < n-1; i++){
-        int p2 = get_median(arr[i], 0);
-        if(p1 == p2) return false;
-        p1 = p2;
-    }
-    return true;
-}
 
 void solve(){
-    cin >> n;
-    a.resize(n);
-    for(int i = 1; i <= n; i++) a[i-1] = i;
-
-    do{
-        if(is_valid(a)){
-            deb_array(a);
-            return;
-        }
-    }while(next_permutation(all(a)));
+    
 }
 
 int main() {
