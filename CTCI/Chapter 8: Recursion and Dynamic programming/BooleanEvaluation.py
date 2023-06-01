17ff42ab6049c60fa651cedfc2dee8ce04fe9db8
vgnshiyer <vgnshiyer@gmail.com>
2023-06-01 12:03:37 -0700

    chore: added memoization to boolean evaluation (ctci)

--- CTCI/Chapter 8: Recursion and Dynamic programming/BooleanEvaluation.py
+++ CTCI/Chapter 8: Recursion and Dynamic programming/BooleanEvaluation.py
@@ -1,6 +1,8 @@
-def countEval(booleanString, result) -> int:
+def countEval(booleanString, result, memo) -> int:
     if len(booleanString) == 0: return 0
     if len(booleanString) == 1: return result
+
+    if booleanString + str(result) in memo: return memo[booleanString + str(result)]
 
     nways = 0
     for i in range(1, len(booleanString), 2):
@@ -8,10 +10,10 @@ def countEval(booleanString, result) -> int:
         left = booleanString[:i]
         right = booleanString[i+1:]
 
-        leftTrue = countEval(left, True)
-        leftFalse = countEval(left, False)
-        rightTrue = countEval(right, True)
-        rightFalse = countEval(right, False)
+        leftTrue = countEval(left, True, memo)
+        leftFalse = countEval(left, False, memo)
+        rightTrue = countEval(right, True, memo)
+        rightFalse = countEval(right, False, memo)
 
         if operator == '^':
             if result == True:
@@ -29,10 +31,13 @@ def countEval(booleanString, result) -> int:
             else:
                 ans = (leftTrue * rightFalse) + (leftFalse * rightTrue) + (leftFalse * rightFalse)
         nways += ans
+
+    memo[booleanString + str(result)] = nways
     return nways
 
 if __name__ == '__main__':
     booleanString = '0&0&0&1^1|0'
-    result = False
+    result = True
 
-    print(countEval(booleanString, result))
+    memo = {}
+    print(countEval(booleanString, result, memo))
