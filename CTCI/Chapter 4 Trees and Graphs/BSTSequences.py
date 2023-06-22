bb22eb503daf33b3528270cec04a7871fb6cc97d
vgnshiyer <vgnshiyer@gmail.com>
2023-06-21 21:57:53 -0700

    add: added alternative intuitive approach to BST sequences problem (CTCI)

--- CTCI/Chapter 4 Trees and Graphs/BSTSequences.py
+++ CTCI/Chapter 4 Trees and Graphs/BSTSequences.py
@@ -21,6 +21,7 @@ def bst_sequences(root: TreeNode):
 
             if len(choices) > 0:
                 sequences = visit(choices)
+                print(sequences)
                 for sequence in sequences:
                     output.append([root.value] + sequence)
             else:
@@ -28,6 +29,38 @@ def bst_sequences(root: TreeNode):
         return output
     
     return visit([root])
+
+def bst_sequences2(root: TreeNode):
+    if not root: return [[]]
+
+    leftSequences = bst_sequences2(root.left)
+    rightSequences = bst_sequences2(root.right)
+
+    for left in leftSequences:
+        for right in rightSequences:
+            allSequences = []
+            weave(left, right, [], allSequences)
+    
+    for sequence in allSequences: sequence.insert(0, root.value)
+
+    return allSequences
+
+def weave(left, right, prefix, result):
+    if not left or not right:
+        result.append(prefix + left + right)
+        return
+
+    leftHead = left.pop(0)
+    prefix.append(leftHead)
+    weave(left, right, prefix, result)
+    prefix.pop()
+    left.insert(0, leftHead)
+
+    rightHead = right.pop(0)
+    prefix.append(rightHead)
+    weave(left, right, prefix, result)
+    prefix.pop()
+    right.insert(0, rightHead)
 
 class BSTTests(unittest.TestCase):
     @staticmethod
@@ -40,7 +73,8 @@ class BSTTests(unittest.TestCase):
 
     def test_bst_sequences(self):
         root = BSTTests.create_tree()
-        sequences = bst_sequences(root)
+        sequences = bst_sequences2(root)
+        print(sequences)
         actual_result = set(tuple(sequence) for sequence in sequences)
         expected_result = set(tuple(item) for item in [[1,2,3,4], [1,3,2,4], [1,3,4,2]])
         self.assertSetEqual(actual_result, expected_result)
