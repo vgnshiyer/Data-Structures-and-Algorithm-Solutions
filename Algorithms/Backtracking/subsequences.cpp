ced3a0e4eab70aac237d32ab2f96da49df84bb15
vgnshiyer <vgnshiyer@gmail.com>
2023-06-22 21:48:10 -0700

    add: compact solution for printing subsequences (backtracking)

--- Algorithms/Backtracking/subsequences.cpp
+++ Algorithms/Backtracking/subsequences.cpp
@@ -12,7 +12,15 @@ void subseq(string s, string ans, int i){
     subseq(s, ans + s[i], i + 1);
 }
 
+void subseq2(string s, string ans){
+    cout << ans << endl;
+    for(int i = 0; i < s.length(); i++)
+        subseq2(s.substr(i+1, s.length()), ans + s[i]);
+}
+
 int main (){
     subseq("abc", "", 0);
+    cout << "\nCompact Version : " << endl;
+    subseq2("abc", "");
     return 0;
 }
