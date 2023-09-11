16d94c3f81116085a6503ac095ad563aeb9f05b5
vgnshiyer <vgnshiyer@asu.edu>
2023-09-11 10:40:09 -0700

    chore: added proof for max chain len greedy solution

--- Algorithms/Dynamic Programming/Linear DP/maxChainLen.py
+++ Algorithms/Dynamic Programming/Linear DP/maxChainLen.py
@@ -21,6 +21,13 @@ def findLongestChain_DP(pairs: List[List[int]]) -> int:
 
 # Greedy approach O(nlogn)
 def findLongestChain_greedy(pairs: List[List[int]]) -> int:
+    '''
+    Why this solution works?
+    Consider pairA and pairB, where pairA[1] < pairB[1].
+    1. If pairA[1] < pairB[0], then we can add pairA and pairB to our chain.
+    2. If pairA[1] >= pairB[0], then we can chain add either pairA or pairB to our chain.
+    But consider pairA[1] < pairB[1]. Given this information, we must include pairA to our chain since it has better opportunity to chain with other pairs in the future since its second element is smaller.
+    '''
     pairs.sort(key=lambda pair: pair[1])
     maxChainLen = 1
     cur = pairs[0][1]
