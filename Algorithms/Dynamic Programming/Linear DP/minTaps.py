3f315719c9a499a441377d1fd8c4807fc5cfef48
vgnshiyer <vgnshiyer@asu.edu>
2023-09-13 14:39:39 -0700

    add: min taps to open to water the garden

---
+++ Algorithms/Dynamic Programming/Linear DP/minTaps.py
@@ -0,0 +1,22 @@
+def minTaps(n: int, ranges: List[int]) -> int:
+    '''
+    state: dp[i] -> min number of taps to open for watering from 0 to i
+    We initialize this array with a large value like infinity for each position. By doing so, we indicate that no taps have been applied yet, and we can consider these positions as "unreachable" until we find a tap that can water them.
+    
+    base case: Since we do not have to open any taps to water a part of zero length, thus dp[0]=0.
+
+    transition: for every tap, we get the start and end of the range. For the end of the range we calculate the least amount of taps to open that fills the range 0 to end.
+    therefor for j in start, end, dp[end] = min(dp[end], 1 + dp[j])
+    '''
+
+    INF = 10**9
+
+    dp = [INF] * (n + 1)
+    dp[0] = 0
+
+    for i in range(n + 1):
+        tap_start = max(0, i - ranges[i])
+        tap_end = min(n, i + ranges[i])
+        for j in range(tap_start, tap_end + 1):
+            dp[tap_end] = min(dp[tap_end], dp[j] + 1)
+    return -1 if dp[-1] == INF else dp[-1]
