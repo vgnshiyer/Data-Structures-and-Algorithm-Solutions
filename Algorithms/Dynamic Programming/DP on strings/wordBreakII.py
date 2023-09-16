034bd5af37a2f40849f9a8950206a80f3befd283
vgnshiyer <vgnshiyer@gmail.com>
2023-09-16 12:57:33 -0700

    add: word break II optimized

--- Algorithms/Dynamic Programming/DP on strings/wordBreakII.py
+++ Algorithms/Dynamic Programming/DP on strings/wordBreakII.py
@@ -12,3 +12,22 @@ def wordBreak(s: str, wordDict: List[str]) -> List[str]:
     
     dfs(0)
     return sentences
+
+def wordBreak_optimized(s: str, wordDict: List[str]) -> List[str]:
+    sentences = []
+    memo = {}
+
+    def dfs(i):
+        if i >= len(s): return ['']
+
+        if i not in memo:
+            res = []
+            for word in wordDict:
+                if s[i:i + len(word)] == word:
+                    rest = dfs(i + len(word))
+                    for w in rest:
+                        res.append((word + ' ' + w).strip())
+            memo[i] = res
+        return memo[i]
+    
+    return dfs(0)
