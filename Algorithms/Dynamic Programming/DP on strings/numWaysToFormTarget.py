1385bbee08dcb682e23a5e988e84ae9eef7eff6e
vgnshiyer <vgnshiyer@gmail.com>
2023-09-19 15:30:19 -0700

    add: num ways to form target iterative

--- Algorithms/Dynamic Programming/DP on strings/numWaysToFormTarget.py
+++ Algorithms/Dynamic Programming/DP on strings/numWaysToFormTarget.py
@@ -27,3 +27,24 @@ def numWays(words: List[str], target: str) -> int:
         return (dfs(i + 1, j) % mod + (mp[i][target[j]] * dfs(i + 1, j + 1) if target[j] in mp[i] else 0) % mod) % mod
 
     return dfs(0, 0) % mod
+
+def numWays_iterative(words: List[str], target: str) -> int:
+    mod = 10 ** 9 + 7
+    n = len(words[0]) ## length of words
+    m = len(target)
+
+    mp = [defaultdict(int) for _ in range(n)]
+    for word in words:
+        for i in range(n):
+            mp[i][word[i]] += 1
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(n + 1): dp[i][0] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            dp[i][j] = dp[i - 1][j] % mod
+            if target[j - 1] in mp[i - 1]:
+                dp[i][j] += (mp[i - 1][target[j - 1]] * dp[i - 1][j - 1]) % mod
+
+    return dp[-1][-1] % mod
