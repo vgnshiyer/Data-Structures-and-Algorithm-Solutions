b237bac10996d13df4f1c45ce45d3a52c633db8c
vgnshiyer <vgnshiyer@gmail.com>
2023-09-28 19:36:58 -0700

    add: min height shelves iterative bottom up approach

--- Algorithms/Dynamic Programming/Linear DP/minHeightShelves.py
+++ Algorithms/Dynamic Programming/Linear DP/minHeightShelves.py
@@ -26,3 +26,24 @@ def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         return min(place_on_current_shelf, place_on_next_shelf)
 
     return helper(0, shelfWidth)
+
+'''
+Notice that the iterative version eliminates two variables from our state.
+These type of problems where we are made to keep track of a max/min value between recursive calls can be simplified with some iterative tricks.
+In this problem, we tried to greedily place books in a shelf while tracking the minimum height of the books placed so far.
+Therefore there was no need to store that information. All we stored and want is, what is the minimum height of the books placed at position j.
+'''
+def minHeightShelves_iterative(books: List[List[int]], shelfWidth: int) -> int:
+    dp = [float('inf')] * (len(books) + 1)
+    dp[0] = 0
+
+    for i in range(1, len(books) + 1):
+        width, height = books[i - 1]
+        dp[i] = dp[i - 1] + height ## place next book on the next shelf
+        j = i - 1
+        while j > 0 and (width + books[j - 1][0]) <= shelfWidth:
+            height = max(height, books[j-1][1])
+            width += books[j-1][0]
+            dp[i] = min(dp[i], dp[j-1] + height)
+            j -= 1
+    return dp[-1]
