3aa15f71ee49f71564add424c8f58674b5b53557
vgnshiyer <vgnshiyer@gmail.com>
2023-10-12 23:20:00 -0700

    add: space optimized house robber III

--- Algorithms/Dynamic Programming/DP on trees /houseRobberIII.py
+++ Algorithms/Dynamic Programming/DP on trees /houseRobberIII.py
@@ -8,3 +8,13 @@ def rob(root: Optional[TreeNode]) -> int:
         return max(n_r, r)
 
     return getMaxProfit(root)
+
+def rob_spaceOptimized(root: Optional[TreeNode]) -> int:
+    @cache
+    def getMaxProfit(node):
+        if not node: return (0, 0)
+
+        left, right = getMaxProfit(node.left), getMaxProfit(node.right)
+        return (max(node.val + left[1] + right[1], left[0] + right[0]), left[0] + right[0])
+
+    return getMaxProfit(root)[0]
