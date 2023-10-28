42f89f12e5e98190c5f6fa7d4e709c3f41f1778b
Vignesh Iyer <39982819+vgnshiyer@users.noreply.github.com>
2023-10-27 17:43:43 -0700

    add: trees notes

--- Algorithms/Graph theory/Trees/rooting_a_graph.py
+++ Algorithms/Graph theory/Trees/rooting_a_graph.py
@@ -2,7 +2,7 @@ import TreeNode
 
 def rootTree(rootId, parent=None):
     for child in neighbors[rootId]:
-        if parent != None and child != parent:
+        if parent != None and child == parent:
             continue
             
         child = TreeNode(child.val, [])
