d83548a17f2eba132f46a792305bbd8f0196c880
vgnshiyer <vgnshiyer@gmail.com>
2023-11-16 18:28:25 -0700

    add: find all recipes (topsort)

---
+++ Algorithms/Graph theory/Topological sort/Problems/findAllRecipes.py
@@ -0,0 +1,21 @@
+def findAllRecipes(recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+    recipe_book = {}
+    for i, item in enumerate(recipes):
+        recipe_book[item] = ingredients[i]
+
+    being_prepared = set()
+    @cache
+    def search(item):
+        if item in supplies: return True # found an item
+        if item not in recipe_book: return False # raw material : must need
+        being_prepared.add(item)
+        for more_item in recipe_book[item]:
+            if more_item in being_prepared: return False
+            if not search(more_item): return False
+        being_prepared.remove(item)
+        return True
+
+    can_make = []
+    for item in recipes:
+        if search(item): can_make.append(item)
+    return can_make
