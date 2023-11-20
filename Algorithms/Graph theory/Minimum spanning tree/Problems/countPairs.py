e4488dd96fbb10cf452aa135014776fc2fd319b2
Vignesh Iyer <39982819+vgnshiyer@users.noreply.github.com>
2023-11-20 06:31:21 -0700

    add: count pairs unreachable (dsu)

---
+++ Algorithms/Graph theory/Minimum spanning tree/Problems/countPairs.py
@@ -0,0 +1,28 @@
+
+def __init__(self, n):
+    self.n = n
+    self.parent = [i for i in range(n)]
+    self.size = [1 for i in range(n)]
+
+def find(self, a):
+    if a != self.parent[a]:
+        self.parent[a] = self.find(self.parent[a])
+    return self.parent[a]
+
+def union(self, a, b):
+    pa, pb = self.find(a), self.find(b)
+    if self.find(a) != self.find(b):
+        self.parent[pa] = self.parent[pb]
+        self.size[pb] += self.size[pa]
+
+class Solution:
+def countPairs(self, n: int, edges: List[List[int]]) -> int:
+    dsu = DSU(n)
+
+    for i, j in edges:
+        dsu.union(i, j)
+
+    ans = 0
+    for i in range(n):
+        ans += (n - dsu.size[dsu.find(i)])
+    return ans // 2
