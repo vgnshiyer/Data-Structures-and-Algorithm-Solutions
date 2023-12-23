1fcabc135001af5302f811acbbdeb7301651dfb6
vgnshiyer <vgnshiyer@gmail.com>
2023-12-23 13:55:58 -0700

    chore: added doc strings to Hash Table

--- CTCI/Chapter 7 Object Oriented Design/Python/hash_table.py
+++ CTCI/Chapter 7 Object Oriented Design/Python/hash_table.py
@@ -10,6 +10,12 @@ class HashMap:
         self._hash_function = lambda x : x % self.size
 
     def set(self, k, v):
+        '''
+        Sets a key val pair in the hash_table.
+
+        If the key gets hashed to the same value as some other key,
+        chains the new value in to the table at the hashed index.
+        '''
         hash_index = self._hash_function(k)
         for item in self.table[hash_index]:
             if item.key == k:
@@ -18,12 +24,24 @@ class HashMap:
         self.table[hash_index].append(Item(k, v))
 
     def get(self, k):
+        '''
+        Gets the value for a given key.
+
+        If key not present in the hashed index,
+        returns an ItemNotFoundException
+        '''
         hash_index = self._hash_function(k)
         for item in self.table[hash_index]:
             if item.key == k: return item.val
         raise ItemNotFoundException
 
     def remove(self, k):
+        '''
+        Removes an item from the hashed index with the given key.
+
+        If item not present at hashed index,
+        returns an ItemNotFoundException
+        '''
         hash_index = self._hash_function(k)
         for index, item in enumerate(self.table[hash_index]):
             if item.key == k:
@@ -32,6 +50,11 @@ class HashMap:
         raise ItemNotFoundException
 
     def __contains__(self, k):
+        '''
+        Implements the python "in" keyword.
+
+        Returns true if an item is present with key == k
+        '''
         hash_index = self._hash_function(k)
         for item in self.table[hash_index]:
             if item.key == k:
