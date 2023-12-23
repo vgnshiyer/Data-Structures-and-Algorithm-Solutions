4543c5e512ad8001e2d8fad8a42cee96643209ba
vgnshiyer <vgnshiyer@gmail.com>
2023-12-23 00:38:06 -0700

    add: hashtable oop design

---
+++ System Design/low_level/hash_table.py
@@ -0,0 +1,32 @@
+class Item:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+
+class HashMap:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+        self._hash_function = lambda x : x % self.size
+
+    def set(self, k, v):
+        hash_index = self._hash_function(k)
+        for item in self.table[hash_index]:
+            if item.key == k:
+                item.val = v
+                return
+        self.table[hash_index].append(Item(k, v))
+
+    def get(self, k):
+        hash_index = self._hash_function(k)
+        for item in self.table[hash_index]:
+            if item.key == k: return item.val
+        raise Exception("Item not found.")
+
+    def remove(self, k):
+        hash_index = self._hash_function(k)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == k:
+                del self.table[hash_index][index]
+                return
+        raise Exception("Item not found.")
