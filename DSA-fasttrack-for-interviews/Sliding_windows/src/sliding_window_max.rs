1832aa5f66b9c47cd565e58cc3ca0c2b497282ef
Vignesh Iyer <vgnshiyer@asu.edu>
2024-08-21 21:42:09 -0700

    sliding window max

--- DSA-fasttrack-for-interviews/Sliding_windows/src/sliding_window_max.rs
+++ DSA-fasttrack-for-interviews/Sliding_windows/src/sliding_window_max.rs
@@ -1,12 +1,17 @@
 use std::collections::VecDeque;
 
 pub fn run(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    let mut q: VecDeque<i32> = VecDeque::new();
+    let mut q: VecDeque<usize> = VecDeque::new();
     let mut result: Vec<i32> = Vec::new();
-    for (i, n) in nums.iter().enumerate() {
-        while !q.is_empty() && *n > q[q.len() - 1] || q.len() > k as usize { q.pop_front(); }
-        q.push_back(*n);
-        if i >= (k - 1) as usize { result.push(q[0]); }
+    let mut l = 0;
+    for r in 0..nums.len() {
+        while !q.is_empty() && nums[*q.back().unwrap()] < nums[r] { q.pop_back(); }
+        q.push_back(r);
+        if (r - l + 1) as i32 == k {
+            result.push(nums[*q.front().unwrap()]);
+            l += 1;
+        }
+        while !q.is_empty() && *q.front().unwrap() < l { q.pop_front(); }
     }
     result
 }
